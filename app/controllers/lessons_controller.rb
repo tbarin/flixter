0eb379ee34aab3187627e45377f388524016ac59
Teresa Barin <tbarin@gmail.com>
2017-07-07 19:43:57 +0000

    added permissions for student lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_in_current_course, only: [:show]
+
   def show
   end
 
@@ -7,5 +10,11 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_enrolled_in_current_course
+    if current_user.enrolled_in?(current_lesson.section.course) == false
+      redirect_to course_path(current_lesson.section.course), alert: 'Please enroll in the current course'
+    end
   end
 end
